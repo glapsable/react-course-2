459aa7ed5eb6a7323ab634ee006486397ff616b8
Glapson <glapsable@gmail.com>
2019-07-05 16:46:33 +0200

    initial commit

---
+++ src/actions/index.js
@@ -0,0 +1,33 @@
+import _ from 'lodash';
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => {
+  return async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+  console.log(getState());
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach(id => dispatch(fetchUser(id)));
+  }
+};
+
+export const fetchPosts = () => {
+  return async function(dispatch, getState) {
+    const response = await jsonPlaceholder.get('/posts');
+
+    dispatch({ type: 'FETCH_POSTS', payload: response.data });
+  }
+};
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+
+//
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+//
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
